20349ec58f7d174e554ac7cd0f0e2bbb63dc3ad8
gimminjae <min356812@naver.com>
2022-08-19 22:50:59 +0900

    QuestionService 구현

---
+++ src/main/java/com/qnasite/question/QuestionService.java
@@ -0,0 +1,16 @@
+package com.qnasite.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return questionRepository.findAll();
+    }
+}
