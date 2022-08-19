93048d02bfdc7fded05ca9edc835c5cec8e0a62a
gimminjae <min356812@naver.com>
2022-08-19 22:45:09 +0900

    root url /question/list 로 연결

--- src/main/java/com/qnasite/MainController.java
+++ src/main/java/com/qnasite/MainController.java
@@ -13,4 +13,9 @@ public class MainController {
     public String index() {
         return "Hello";
     }
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 }
