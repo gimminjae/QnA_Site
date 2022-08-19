f4def8883fa77e83df11a81ff60a61088aa2d876
gimminjae <min356812@naver.com>
2022-08-19 20:58:29 +0900

    setting

---
+++ src/main/java/com/qnasite/QnASiteApplication.java
@@ -0,0 +1,13 @@
+package com.qnasite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnASiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QnASiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qnasite/QnASiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnasite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnASiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
