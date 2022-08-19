d1536a7168b2ba778e35559b71e7e07cd4f467ef
gimminjae <min356812@naver.com>
2022-08-19 23:02:47 +0900

    질문 상세 페이지 구현(+DataNotFoundException구현)

--- src/main/java/com/qnasite/question/QuestionController.java
+++ src/main/java/com/qnasite/question/QuestionController.java
@@ -3,6 +3,7 @@ package com.qnasite.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,5 +17,11 @@ public class QuestionController {
         List<Question> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+    @RequestMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Long id) {
+        Question question = questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/qnasite/question/QuestionService.java
+++ src/main/java/com/qnasite/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.qnasite.question;
 
+import com.qnasite.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +14,13 @@ public class QuestionService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+    public Question getQuestion(long id) {
+        Optional<Question> optionalQuestion = questionRepository.findById(id);
+        if(optionalQuestion.isPresent()) {
+            return optionalQuestion.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
