ab215e41fbae84d92ce37b4e5a7453350cb14bfb
gimminjae <min356812@naver.com>
2022-08-20 16:09:32 +0900

    PasswordEncoder @Bean 등록

--- src/main/java/com/qnasite/user/UserService.java
+++ src/main/java/com/qnasite/user/UserService.java
@@ -2,18 +2,19 @@ package com.qnasite.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String password, String email) {
         SiteUser siteUser = new SiteUser();
         siteUser.setUsername(username);
         siteUser.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         siteUser.setPassword(passwordEncoder.encode(password));
 
         userRepository.save(siteUser);
