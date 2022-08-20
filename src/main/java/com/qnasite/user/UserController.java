ff2bd263daf7ec929c2b10cbf4c68f5525ef6f35
gimminjae <min356812@naver.com>
2022-08-20 16:19:24 +0900

    UserController 구현 : signup(Get, Post) 매핑 구현

---
+++ src/main/java/com/qnasite/user/UserController.java
@@ -0,0 +1,35 @@
+package com.qnasite.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm) {
+        return "sign_up";
+    }
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+        if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("passwords", "passwordInCorrect", "2개의 패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+        userService.create(userCreateForm.getUsername(), userCreateForm.getPassword1(), userCreateForm.getEmail());
+
+        return "redirect:/";
+    }
+}
