b37dc41a6f5424657c0b61b8a65fd5e86b334e3f
gimminjae <min356812@naver.com>
2022-08-20 15:26:02 +0900

    SecurityConfig 구현

---
+++ src/main/java/com/qnasite/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.qnasite;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity //모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애너테이션
+@Configuration //스프링의 환경설정 파일임을 의미하는 애너테이션
+public class SecurityConfig {
+    //@EnableWebSecurity 애너테이션을 사용하면 내부적으로 SpringSecurityFilterChain이 동작하여 URL 필터가 적용된다.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeRequests().antMatchers("/**").permitAll();
+
+        return httpSecurity.build();
+    }
+
+}
