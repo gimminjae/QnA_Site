a6260c412343d988e997b141eff940bc44bf9e20
gimminjae <min356812@naver.com>
2022-08-22 22:04:38 +0900

    답변 수정 구현 완료

--- src/main/java/com/qnasite/answer/AnswerController.java
+++ src/main/java/com/qnasite/answer/AnswerController.java
@@ -1,18 +1,18 @@
 package com.qnasite.answer;
 
 import com.qnasite.question.Question;
+import com.qnasite.question.QuestionForm;
 import com.qnasite.question.QuestionService;
 import com.qnasite.user.SiteUser;
 import com.qnasite.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -35,5 +35,30 @@ public class AnswerController {
         }
         answerService.create(question, answerForm.getContent(), siteUser);
         return "redirect:/question/detail/%s".formatted(id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String modifyAnswer(AnswerForm answerForm, @PathVariable("id") Long id, Principal principal) {
+        Answer answer = answerService.getAnswer(id);
+        if(!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        answerForm.setContent(answer.getContent());
+        return "answer_form";
+    }
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String modifyAnswer(@Valid AnswerForm answerForm, BindingResult bindingResult,
+                                 @PathVariable("id") Long id, Principal principal) {
+        if(bindingResult.hasErrors()) return "answer_form";
+
+        Answer answer = answerService.getAnswer(id);
+
+        if(!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        answerService.modify(answer, answerForm.getContent());
+        return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
     }
 }
--- src/main/java/com/qnasite/answer/AnswerService.java
+++ src/main/java/com/qnasite/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.qnasite.answer;
 
+import com.qnasite.DataNotFoundException;
 import com.qnasite.question.Question;
 import com.qnasite.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +20,19 @@ public class AnswerService {
         answer.setCreateDate(LocalDateTime.now());
         answer.setAuthor(siteuser);
         answer.setQuestion(question);
+        answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Long id) {
+        Optional<Answer> optionalAnswer = answerRepository.findById(id);
+        if(optionalAnswer.isPresent()) {
+            return optionalAnswer.get();
+        } else throw new DataNotFoundException("answer ont found");
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
         answerRepository.save(answer);
     }
 }
