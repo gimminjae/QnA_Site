50a129864fe875608c4c19099de32fc9ff9ff1b5
gimminjae <min356812@naver.com>
2022-08-22 17:17:43 +0900

    Question, Answer에 author 속성 추가

--- src/main/java/com/qnasite/answer/Answer.java
+++ src/main/java/com/qnasite/answer/Answer.java
@@ -1,6 +1,7 @@
 package com.qnasite.answer;
 
 import com.qnasite.question.Question;
+import com.qnasite.user.SiteUser;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,4 +23,7 @@ public class Answer {
 
     @ManyToOne
     private Question question;
+
+    @ManyToOne
+    private SiteUser author;
 }
