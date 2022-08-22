d7e2d60e1663a51b3333bd40225825c853f3eb7e
gimminjae <min356812@naver.com>
2022-08-22 22:12:42 +0900

    질문 삭제 구현 완료

--- src/main/java/com/qnasite/answer/AnswerController.java
+++ src/main/java/com/qnasite/answer/AnswerController.java
@@ -61,4 +61,14 @@ public class AnswerController {
         answerService.modify(answer, answerForm.getContent());
         return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
     }
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String deleteAnswer(@PathVariable("id") Long id, Principal principal) {
+        Answer answer = answerService.getAnswer(id);
+        if(!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        answerService.delete(answer);
+        return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
+    }
 }
--- src/main/java/com/qnasite/answer/AnswerService.java
+++ src/main/java/com/qnasite/answer/AnswerService.java
@@ -35,4 +35,8 @@ public class AnswerService {
         answer.setModifyDate(LocalDateTime.now());
         answerRepository.save(answer);
     }
+
+    public void delete(Answer answer) {
+        answerRepository.delete(answer);
+    }
 }
